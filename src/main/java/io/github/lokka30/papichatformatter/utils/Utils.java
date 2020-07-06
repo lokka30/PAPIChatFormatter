a2e8ff1046a346cc4bf852a0157bb2dd4f8b1e54
lokka30 <lokka30@protonmail.com>
2020-07-07 06:47:57 +0800

    v1.0.3-RELEASE

--- src/main/java/io/github/lokka30/papichatformatter/utils/Utils.java
+++ src/main/java/io/github/lokka30/papichatformatter/utils/Utils.java
@@ -1,14 +1,11 @@
 package io.github.lokka30.papichatformatter.utils;
 
-import io.github.lokka30.papichatformatter.PAPIChatFormatter;
-
 import java.util.Arrays;
 import java.util.List;
 
 public class Utils {
 
-    public Utils() {
-    }
+    public Utils() { }
 
     public List<String> getSupportedServerVersions() {
         return Arrays.asList(
